43b8e6737e7a367aeda2135a56ea0167593b8e5f
Seydalievnikolay <nikolay-seydalie@yandex.ru>
2022-12-19 21:37:02 +0700

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,85 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+    }
+    public static void task1(){
+        System.out.println("Задача1");
+        int[] number = new int [3];
+        number[0] = 1;
+        int numberOne = number[0];
+        System.out.println(numberOne);
+        number[1] = 2;
+        int numberTwo = number[1];
+        System.out.println(numberTwo);
+        number[2] = 3;
+        int numberThree = number[2];
+        System.out.println(numberThree);
+
+        double [] fraction = {1.57, 7.654, 9.986};
+        System.out.println(fraction[0]);
+        System.out.println(fraction[1]);
+        System.out.println(fraction[2]);
+
+        double [] numberPi = {1};
+        numberPi [0] = 3.1415926535;
+        System.out.println(numberPi[0]);
+    }
+    public static void task2(){
+        System.out.println("Задача2");
+        int[] number = new int [3];
+        number[0] = 1;
+        int numberOne = number[0];
+        number[1] = 2;
+        int numberTwo = number[1];
+        number[2] = 3;
+        int numberThree = number[2];
+        System.out.println(numberOne+ ", " +numberTwo+ ", " +numberThree);
+
+        double [] fraction = {1.57, 7.654, 9.986};
+        System.out.println(fraction[0] +", "+ fraction[1] +", "+ fraction[2]);
+
+        double [] numberPi = {1};
+        numberPi [0] = 3.1415926535;
+        System.out.println(numberPi[0]);
+        
+    }
+    public static void task3(){
+        System.out.println("Задача3");
+        int[] number = new int [3];
+        number[2] = 1;
+        int numberOne = number[2];
+        number[1] = 2;
+        int numberTwo = number[1];
+        number[0] = 3;
+        int numberThree = number[0];
+        System.out.println(numberThree+ ", " +numberTwo+ ", " +numberOne);
+
+        double [] fraction = {1.57, 7.654, 9.986};
+        System.out.println(fraction[2] +", "+ fraction[1] +", "+ fraction[0]);
+
+        double [] numberPi = {1};
+        numberPi [0] = 3.1415926535;
+        System.out.println(numberPi[0]);
+    }
+    public static void task4(){
+        System.out.println("Задача4");
+        int [] numbers = {1, 2, 3};
+        numbers[0] = 1;
+        int numberOne = numbers[0];
+        numbers[1] = 2;
+        int numberTwo = numbers[1];
+        numbers[2] = 3;
+        int numberThree = numbers[2];
+        System.out.println(Arrays.toString(numbers));
+        for (numbers=0; numbers <= 1; numbers++) {
+            System.out.println(numbers[0] + "," + numbers[1] + ", " + numbers[2]);
+
+
+        }
     }
 }
